19c0840e389f43475609ceddafc3eae89d2da77c
dayorolands <iadekoya@appzonegroup.com>
2024-07-03 20:03:45 +0100

    Initial Commit

---
+++ wk1/wk1App.swift
@@ -0,0 +1,17 @@
+//
+//  wk1App.swift
+//  wk1
+//
+//  Created by Dayo Adekoya on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct wk1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
