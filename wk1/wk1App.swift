de94ecc1d45c047851a1e778e22cb332a8e010a4
Ifedayo Adekoya <47520140+dayorolands@users.noreply.github.com>
2024-07-06 10:37:08 +0100

    Handle ability to select your favorite park

--- wk1/wk1App.swift
+++ wk1/wk1App.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct wk1App: App {
+    @State private var modelData = ModelData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)
         }
     }
 }
